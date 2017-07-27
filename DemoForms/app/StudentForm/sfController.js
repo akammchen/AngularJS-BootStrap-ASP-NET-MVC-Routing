3937aacb50ccaacdbe43757b9d5890c2bf784d8a
Kishor Bikram Oli <olikishor@outlook.com>
2017-07-27 10:10:06 +0545

    project added wih routing implementation

---
+++ DemoForms/app/StudentForm/sfController.js
@@ -0,0 +1,15 @@
+﻿studentFormsApp.controller('sfController',
+    function sfController($scope, sfService) {
+        $scope.student = sfService.student;
+
+        $scope.departments = [
+               "Math",
+               "Physics",
+               "Chemistry",
+               "English"
+        ];
+        $scope.submitForm = function () {
+            alert("Form submitted");
+            //AJAX calls, validations, will be covered later.
+        }
+    });
